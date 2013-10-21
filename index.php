99b4ce469f7b97ff56f679659d72f6bfd22429f1
Greg Koestler <gkoes3@gmail.com>
2013-10-21 11:06:46 -0400

    Started book upload form

--- index.php
+++ index.php
@@ -2,14 +2,12 @@
 //Connect to the database
 require_once("db_config/opendb.php");
 ?>
-
 <html>
 <head>
-	<title>
-		Hello
-	</title>
+	<title>311 Project</title>
 </head>
 <body>
 <?php echo "Hello world! <br />"; ?>
+<a href="/uploading/upload_book_form.php">Upload a textbook</a>
 </body>
 </html>
---
+++ uploading/upload_book_form.php
@@ -0,0 +1,6 @@
+<form action="process_book_upload.php" method="post">
+	Book title: <input type="text" name="book_name"/> <br />
+	ISBN number: <input type="text" name="isbn_number" /> <br />
+	Asking price: <input type="text" name="book_price" /> <br /> 	
+	<input type="submit" />
+</form>
