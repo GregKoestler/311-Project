49e79aab6c0c53027157ba4b86fed3542078d9f4
Shane Phibbs <phibbss@uwindsor.ca>
2013-10-23 21:59:24 -0400

    Merge branch 'master' of https://github.com/GregKoestler/311-Project
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -1,15 +1,51 @@
 <?php
 //Connect to the database
 require_once("db_config/opendb.php");
+
+
+// Get all textbooks
+$textbookRetrievalSql = "select id, book_title, book_author, isbn_number, book_price, date_added from Textbooks";
+$textbookRetrievalResult = mysql_query($textbookRetrievalSql);
+
 ?>
+
 <html>
 <head>
-	<title>311 Project</title>
+	<title>University of Windsor Used Book Store</title>
+
+	<link rel="stylesheet" href="main.css" type="text/css" /> 
 </head>
 <body>
-<?php echo "Hello world! <br />"; ?>
-<a href="/uploading/upload_book_form.php">Upload a textbook</a>
-<a href="/user_auth/user_signup.php">Sign Up</a>
-<a href="/user_auth/user_signin.php">Login</a>
+<header id="banner">
+	<h1>University of Windsor Used Book Store</h1>
+	<nav><ul>
+		<li><a href="#">Home</a></li>
+		<li><a href="/upload_book/upload_book_form.php">Upload a textbook</a></li>
+		<li><a href="/user_auth/user_signup.php">Sign Up</a></li>
+		<li><a href="/user_auth/user_signin.php">Login</a></li>
+	</ul></nav>
+
+</header>
+
+<table id="mainTable">
+	<tbody>
+	<?php while($textbook = mysql_fetch_assoc($textbookRetrievalResult)): ?>
+		<tr>
+			<td>
+				<a><?php echo $textbook['book_title']?></a> <!-- Link to page displaying all book details -->
+				<br>
+				Description....
+			</td>
+			<td>
+				$<?php echo $textbook['book_price']?>
+			</td>
+			<td>
+				<?php echo $textbook['date_added']?>
+			</td>
+		</tr>
+	<?php endwhile;?>
+	</tbody>
+</table>
+
 </body>
 </html>
