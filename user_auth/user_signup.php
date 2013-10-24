697c2b06e7792c98ca3298ba07c85e3257de4cbc
Jeffrey Pare <jpare16@gmail.com>
2013-10-23 22:52:20 -0400

    Update user_signup.php (conformed to DB decsription)

--- user_auth/user_signup.php
+++ user_auth/user_signup.php
@@ -2,15 +2,16 @@
 	require_once ("../db_config/opendb.php");
 	
 	$username = mysql_real_escape_string($_POST['uwin_id']);
-	$full_name = mysql_real_escape_string($_POST['name']);
+	$firstname = mysql_real_escape_string($_POST['first_name']);
+	$lastname = mysql_real_escape_string($_POST['last_name']);
 	$password = mysql_real_escape_string($_POST['pass']);
 	$confirm = mysql_real_escape_string($_POST['confirm']);
 	
 	if ($password !== $confirm) {
-		header ("location: user_signup.php?conf=0");		
+		header ("location: user_signup.php?conf=1");		
 	}
 	else {
-		mysql_query("INSERT INTO users(id, name, password) VALUES ('$username', '$full_name', '$password')");
+		mysql_query("INSERT INTO users(fname, lname, id, password) VALUES ($firstname', '$lastname', '$username', '$password')");
 	//header("location: index.php?auth=1");
 	//mysql_close ($conn);
 	}
@@ -22,11 +23,12 @@
 </head>
 <body>
 	<form action="user_signup.php" method="post">
-		Name<br /><input type="text" name="name" /><br />
+		First name<br /><input type="text" name="first_name" /><br />
+		Last name<br /><input type="text" name="last_name" /><br />
 		UWin ID<br /><input type="text" name="uwin_id" />@uwindsor.ca<br />
 		Password<br /><input type="password" name="pass" /><br />
 		Confirm password<br /><input type="password" name="confirm" />
-			<?php if ($_GET['conf'] == 0) echo "Passwords do not match!"; ?><br />
+			<?php if ($_GET['conf'] == 1) echo "Passwords do not match!"; ?><br />
 		<input type="submit" value="Register"/>
 	</form>
 </body>
